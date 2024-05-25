acdeb491ed51745ddc9ab591179e91cbe0051d85
Shmuel Levinson <levinson.shmuel@gmail.com>
2024-05-25 22:57:42 +0300

    Added many files; implemented end-to-end expense creation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import { log } from "console";
 import { Client } from "pg";
 import sql from "sql-bricks";
 import cors from "cors";
+import { Expense } from "./models";
 dotenv.config();
 const client = new Client(process.env.DATABASE_URL);
 
@@ -12,6 +13,19 @@ async function sanityCheck() {
 	const results = await client.query("SELECT NOW()");
 	console.log(results.rows);
 }
+
+async function getAllUsers() {
+	try {
+		const query = `SELECT * FROM users`;
+		// const query = sql.select().from('users').toString()
+		const results = await client.query(query);
+		return results.rows;
+	} catch (err) {
+		console.error("Error getting users:", err);
+		throw err;
+	}
+}
+
 const initDb = async () => {
 	await client.connect();
 	try {
@@ -19,7 +33,7 @@ const initDb = async () => {
 	} catch (err) {
 		console.error("error executing query:", err);
 	} finally {
-		client.end();
+		// client.end();
 	}
 };
 
@@ -43,6 +57,26 @@ app.use((req: Request, res: Response, next: Function) => {
 
 app.get("/ping", (req: Request, res: Response) => {
 	res.send({ response: "pong", date: new Date() });
+});
+
+app.post("/expenses", async (req: Request, res: Response) => {
+	const expense = req.body as Expense;
+	console.log("expense", expense);
+	res.send(`Expense ${expense.amount} added!`);
+	try {
+		const query = sql
+			.insert("expenses", { amount: expense.amount, user_id: expense.user_id })
+			.toString();
+		const result = await client.query(query);
+		console.log(result.rows);
+	} catch (err) {
+		console.error("Error creating expense:", err);
+	}
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+	const allUsers = await getAllUsers();
+	res.send(allUsers);
 });
 
 app.listen(port, () => {
